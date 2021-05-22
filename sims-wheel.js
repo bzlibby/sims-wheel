c25544b1b7397c1bb82036072d3381fbfa324f1f
bzlibby <b.z.libby@gmail.com>
2021-05-22 15:05:42 -0500

    added checklist generated by ep, gp, sp, and kit arrays

--- sims-wheel.js
+++ sims-wheel.js
@@ -19,7 +19,27 @@ function addPacks () {
 
 function createPackList (parent, content) {
 	const packDiv = document.createElement('div');
-	packDiv.textContent = JSON.stringify(content);
+	for (let i = 0; i < content.length; i++) {
+		// create the checkbox
+		const checkbox = document.createElement('input');
+		checkbox.type = 'checkbox';
+		checkbox.name = content[i];
+		checkbox.id = content[i];
+		// create the label for the checkbox
+		const label = document.createElement('label');
+		label.htmlFor = content[i];
+		label.appendChild(document.createTextNode(content[i]));
+		// add a line break
+		const br = document.createElement('br');
+		// add checkbox item to page
+		packDiv.appendChild(checkbox);
+		packDiv.appendChild(label);
+		packDiv.appendChild(br);
+	}
 	const currentDiv = document.getElementById(parent);
 	currentDiv.append(packDiv);
+}
+
+function createCheckbox () {
+
 }
